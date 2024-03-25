b8ecdbe8329bc8fcdb52cdff4249b2db0f98b3cc
Han-D-Peter <secret_wings@naver.com>
2024-03-25 12:39:02 +0900

    chore: zod

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,9 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
 import { createLazyFileRoute } from "@tanstack/react-router";
+import { z } from "zod";
 
 export const Route = createLazyFileRoute("/")({
   component: About,
 });
 
+const testScheme = z.object({
+  status: z.number(),
+  msg: z.string(),
+});
+
+type Test = z.infer<typeof testScheme>;
+
 function About() {
+  const { data } = useQuery<Test>({
+    queryKey: ["test"],
+    queryFn: () => fetch("/test").then((res) => res.json()),
+  });
+
+  if (data) {
+    testScheme.parse(data);
+  }
+
+  console.log("data", data);
+
+  console.log("data", data);
   return <div className="p-2">Hello from About!</div>;
 }
