01167f967dd24df7cbd7055a9fa6277c5f94cb40
Han-D-Peter <secret_wings@naver.com>
2024-03-28 14:50:53 +0900

    chore: storybook clean up

---
+++ src/app.css.ts
@@ -0,0 +1,5 @@
+import { globalStyle } from "@vanilla-extract/css";
+
+globalStyle("html, body", {
+  padding: "0 20% 0 20%",
+});
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,20 +1,11 @@
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import Header from "../domains/Header/Header";
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>{" "}
-        <Link to="/about" className="[&.active]:font-bold">
-          About
-        </Link>{" "}
-        <Link to="/detail" className="[&.active]:font-bold">
-          detail
-        </Link>
-      </div>
+      <Header />
       <hr />
       <Outlet />
       <TanStackRouterDevtools />
