163083b61f4737c9fd9948ee9b2c0e09e952fb2b
Han-D-Peter <secret_wings@naver.com>
2024-04-04 08:53:18 +0900

    refactor 목 워커 시작 함수 관심사 이동 분리

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { createRouter, RouterProvider } from "@tanstack/react-router";
-
-import { routeTree } from "./routeTree.gen.ts";
+import { RouterProvider } from "@tanstack/react-router";
 import { router } from "./routes/config.ts";
 import { queryClient } from "./domains/shared/queryHook/config.ts";
-
-async function enableMocking() {
-  if (process.env.NODE_ENV !== "development") {
-    return;
-  }
-
-  const { worker } = await import("./mocks/browser");
-
-  // `worker.start()` returns a Promise that resolves
-  // once the Service Worker is up and ready to intercept requests.
-  return worker.start();
-}
+import { enableMocking } from "./mocks/browser.ts";
 
 enableMocking().then(() => {
   ReactDOM.createRoot(document.getElementById("root")!).render(
--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -2,3 +2,11 @@ import { setupWorker } from "msw/browser";
 import { handlers } from "./handlers";
 
 export const worker = setupWorker(...handlers);
+
+export async function enableMocking() {
+  if (process.env.NODE_ENV !== "development") {
+    return;
+  }
+
+  return worker.start();
+}
